b2a8560dde81706352dea814f037a5bfa33af539
Zhou Ziheng <zhou.ziheng@outlook.com>
2023-03-12 18:03:30 +0800

    Refactor out swipe to call widget

--- lib/emergency.dart
+++ lib/emergency.dart
@@ -23,49 +23,65 @@ class Emergency extends StatelessWidget {
             Expanded(child: ListView.builder(itemBuilder: (context, index) {
               return Card(
                   child: ListTile(
-                    onTap: (){
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  InstructionsPage()));
-                    },
-                  // leading: FlutterLogo(),
-                  title: Text('Lorem Ipsum'),
-                  trailing: Icon(Icons.more_vert),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => InstructionsPage()));
+                },
+                // leading: FlutterLogo(),
+                title: Text('Lorem Ipsum'),
+                trailing: Icon(Icons.more_vert),
               ));
             })),
-            Container(
-              height: 80,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(4),
-                child: isSwiped
-                    ? SlideAction(
-                        borderRadius: 12,
-                        elevation: 0,
-                        animationDuration: const Duration(milliseconds: 0),
-                        innerColor: Colors.white,
-                        outerColor: Colors.red,
-                        sliderButtonIcon: const Icon(Icons.call),
-                        text: "Swipe to call \n emergency services",
-                        textStyle:
-                            const TextStyle(fontSize: 20, color: Colors.white),
-                        sliderRotate: false,
-                        onSubmit: () {
-                          // setState(() {
-                          //   isSwiped = false;
-                          // });
-                        },
-                      )
-                    : const Text("hello"),
-              ),
-            ),
+            EmergencySwipeToCall(isSwiped: isSwiped),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class EmergencySwipeToCall extends StatelessWidget {
+  const EmergencySwipeToCall({
+    super.key,
+    required this.isSwiped,
+  });
+
+  final bool isSwiped;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: Colors.red,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: isSwiped
+            ? SlideAction(
+                borderRadius: 12,
+                elevation: 0,
+                animationDuration: const Duration(milliseconds: 0),
+                innerColor: Colors.white,
+                outerColor: Colors.red,
+                sliderButtonIcon: const Icon(Icons.call),
+                text: "            Swipe to call emergency services",
+                textStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                sliderRotate: false,
+                onSubmit: () {
+                  // setState(() {
+                  //   isSwiped = false;
+                  // });
+                },
+              )
+            : const Text("hello"),
       ),
     );
   }
