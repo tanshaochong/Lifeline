c0b7161db73bf82a600f2c3df41dc5b91fc2b101
xujialu <101682270+3nityR@users.noreply.github.com>
2023-03-31 14:15:36 +0800

    Modified swatch colours and accent colours

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,29 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'First Aid App',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Palette.kToDark,
         fontFamily: GoogleFonts.poppins().fontFamily,
       ),
       // home: const HomeView());
       home: const login_tree(),
     );
   }
+}
+
+class Palette {
+  static const MaterialColor kToDark = MaterialColor(
+    0xffde5246, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
+    <int, Color>{
+      50: Color(0xffde5246), //10%
+      100: Color(0xffe16359), //20%
+      200: Color(0xffe5756b), //30%
+      300: Color(0xffe8867e), //40%
+      400: Color(0xffeb9790), //50%
+      500: Color(0xffefa9a3), //60%
+      600: Color(0xfff2bab5), //70%
+      700: Color(0xfff5cbc8), //80%
+      800: Color(0xfffceeed), //90%
+      900: Color(0xffffffff), //100%
+    },
+  );
 }
